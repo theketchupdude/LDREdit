4d788289796d524a30194c7ec38b71fd2facf28b
theketchupdude <38171793+theketchupdude@users.noreply.github.com>
2018-11-26 09:42:34 -0800

    Create setup.js

---
+++ js/setup.js
@@ -0,0 +1,5 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
+
+document.body.appendChild(renderer.domElement);
