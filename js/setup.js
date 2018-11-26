4e2d176c9d4cbc7e31a915476abe3e7b50a51680
theketchupdude <38171793+theketchupdude@users.noreply.github.com>
2018-11-26 10:04:06 -0800

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,12 +1,26 @@
 var scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
+
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+var material = new THREE.MeshPhongMaterial({ color: 0x00ff00, side: THREE.DoubleSide});
+
 var cube = new THREE.Mesh(geometry, material);
+cube.castShadow = true;
+cube.recieveShadow = true;
 scene.add(cube);
+
+var light = new THREE.SpotLight(0xffffff, 1.0);
+light.position.set(0, 500, 0);
+light.castShadow = true;
+scene.add(light);
+
+var sky = new THREE.HemisphereLight(0xffffbb, 0x080820, 2);
+scene.add(sky);
 
 camera.position.z = 5;
